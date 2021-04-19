9d84fcdcc11f876a3d13c97c6da6370799484dc6
MajesticSpiral <49726772+MajesticSpiral@users.noreply.github.com>
2021-04-19 16:50:33 +0100

    Add files via upload
    Interface used in the program

---
+++ ICitySearch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekgemApp
+{
+	using System.Collections.Generic;
+
+	public interface ICityResult
+	{   
+		ICollection<string> NextLetters { get; set; }
+
+		ICollection<string> NextCities { get; set; }
+	}
+}
+
+namespace TekgemApp
+{
+	public interface ICityFinder
+	{
+		ICityResult Search(string searchString);
+	}
+}
